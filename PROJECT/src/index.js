361dba35217ceed60f27e15feb1638fb708048d6
shepijcanwu <shepijcanwu@tencent.com>
2023-08-28 17:18:55 +0800

    feat: 添加测试浏览器规则

--- PROJECT/src/index.js
+++ PROJECT/src/index.js
@@ -1,12 +1,19 @@
-import _ from "lodash";
+// import "./index.css";
+// import _ from "lodash";
 
-function component() {
-  const element = document.createElement("div");
+// function component() {
+//   const element = document.createElement("div");
 
-  // Lodash, currently included via a script, is required for this line to work
-  element.innerHTML = _.join(["Hello", "webpack"], " ");
+//   // Lodash, currently included via a script, is required for this line to work
+//   element.innerHTML = _.join(["Hello", "webpack"], " ");
 
-  return element;
+//   return element;
+// }
+
+// document.body.appendChild(component());
+
+function DOMContentLoaded() {
+  console.log("文档加载完成");
 }
 
-document.body.appendChild(component());
+window.addEventListener("DOMContentLoaded", DOMContentLoaded);
--- PROJECT/webpack.config.js
+++ PROJECT/webpack.config.js
@@ -1,4 +1,5 @@
 const path = require("path");
+// const { webpack } = require("webpack");
 
 module.exports = {
   entry: "./src/index.js",
@@ -6,4 +7,8 @@ module.exports = {
     filename: "main.js",
     path: path.resolve(__dirname, "dist"),
   },
+  devServer: {
+    static: "./dist",
+  },
+  mode: "development",
 };
