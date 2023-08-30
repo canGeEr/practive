4670def4ccb570944e58cd2193a5a7348344d7d9
shepijcanwu <shepijcanwu@tencent.com>
2023-08-30 21:55:55 +0800

    feat: 实现x-平方根的牛顿迭代法

--- 数学/69-x的平方根/index.js
+++ 数学/69-x的平方根/index.js
@@ -2,7 +2,7 @@
  * @param {number} x
  * @return {number}
  */
-var mySqrt = function (x) {
+function mySqrt(x) {
   if (!x) return 0;
   let left = 1,
     right = x;
@@ -20,7 +20,25 @@ var mySqrt = function (x) {
     }
   }
   return target;
-};
+}
+
+/**
+ *
+ * @param {string} x
+ */
+function mySqrt(x) {
+  if (x === 0) return x;
+  let xi = x;
+  const C = x;
+  while (true) {
+    const newXi = 0.5 * (xi + C / xi);
+    if (Math.abs(xi - newXi) < 1e-7) {
+      break;
+    }
+    xi = newXi;
+  }
+  return Math.floor(xi);
+}
 
 const test = [4, 8, 0, 1, 9];
 
