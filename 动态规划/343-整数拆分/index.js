0357ee8f326a4e4aa6b2bbba1f1f3ff4eae6e132
shepijcanwu <shepijcanwu@tencent.com>
2023-09-01 21:03:02 +0800

    feat: 整数拆分

---
+++ 动态规划/343-整数拆分/index.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var integerBreak = function (n) {
+  const dp = new Array(n + 1).fill(0);
+  for (let i = 2; i <= n; i++) {
+    let max = 0;
+    for (let j = 1; j < i; j++) {
+      max = Math.max(max, Math.max(j * (i - j), j * dp[i - j]));
+    }
+    dp[i] = max;
+  }
+  return dp[n];
+};
+
+/**
+ * 考点，动态规划
+ */
