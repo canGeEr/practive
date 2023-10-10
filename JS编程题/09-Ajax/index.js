b9511571dd040ff8161652a479ef87b154345c60
shepijcanwu <shepijcanwu@tencent.com>
2023-10-10 17:29:29 +0800

    feat: 完成ajax变成

--- JS编程题/09-Ajax/index.js
+++ JS编程题/09-Ajax/index.js
@@ -1,15 +1,35 @@
-const xhr = new XMLHttpRequest();
-// 设置请求头
-xhr.setRequestHeader("Content-type", "application/json");
-// 确认开启异步
-xhr.open("get", "url", true);
-// 是否携带cookie
-xhr.withCredentials = true;
-// 尽量在发送时间前挂载好监听事件
-xhr.onreadystatechange = (event) => {
-  const { readyState, status, responseText } = xhr;
-  if (!(readyState === 4 && status === 200)) return;
-  console.log(responseText);
-};
-// 发送数据，注意之类是否需要序列化, encodeURLComponent
-xhr.send();
+function ajax(url, option = {}) {
+  url = option.url || url;
+  const { method, params, query, onSuccess, onFail, headers, withCredentials } =
+    option;
+  const xhr = new XMLHttpRequest();
+  // 设置请求头
+  Object.keys(headers).map((header) => {
+    xhr.setRequestHeader(header, headers[header]);
+  });
+  // 是否携带凭证信息
+  xhr.withCredentials = withCredentials;
+  // 先添加时间监听
+  xhr.addEventListener("readystatechange", function () {
+    const { readyState, status, responseText } = xhr;
+    if (readyState !== 4) return;
+    if (status === 200) {
+      onSuccess(JSON.parse(responseText));
+    } else {
+      onFail(xhr);
+    }
+  });
+  // 初始化方法，是否异步
+  xhr.open(method, url, true);
+  // 真正发送请求到服务端
+  xhr.send(params instanceof FormData ? params : serialize(params));
+}
+
+function serialize(params) {
+  return Object.keys(params)
+    .reduce((result, key) => {
+      result.push(`${key}=${encodeURIComponent(params[key])}`);
+      return result;
+    }, [])
+    .join("&");
+}
